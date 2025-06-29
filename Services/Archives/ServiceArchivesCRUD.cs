c8d21dea69e5c4fc098105ef5a26cf4b657829e6
Anis-Al <anis.algiers@gmail.com>
2025-06-29 01:47:58 +0100

    v15

--- Services/Archives/ServiceArchivesCRUD.cs
+++ Services/Archives/ServiceArchivesCRUD.cs
@@ -1,6 +1,7 @@
 using DCCR_SERVER.Context;
 using DCCR_SERVER.DTOs.Archives;
 using DCCR_SERVER.Models.Principaux.Archives;
+using DCCR_SERVER.Models.Statiques.TablesDomaines;
 using Microsoft.EntityFrameworkCore;
 
 namespace DCCR_SERVER.Services.Archives
@@ -27,13 +28,17 @@ namespace DCCR_SERVER.Services.Archives
 
                 var fichiers = await requete
                     .OrderByDescending(f => f.date_heure_integration_excel)
-                    .Select(f => new ArchiveDto
+                    .Join(_contexte.utilisateurs,
+                        f => f.id_integrateur_excel,
+                        u => u.matricule,
+                        (f, u) => new { Fichier = f, Utilisateur = u })
+                    .Select(x => new ArchiveDto
                     {
-                        IdFichierExcel = f.id_fichier_excel,
-                        NomFichierExcel = f.nom_fichier_excel,
-                        CheminFichierExcel = f.chemin_fichier_excel,
-                        DateHeureIntegrationExcel = f.date_heure_integration_excel,
-                        Integrateur = f.id_integrateur_excel
+                        IdFichierExcel = x.Fichier.id_fichier_excel,
+                        NomFichierExcel = x.Fichier.nom_fichier_excel,
+                        CheminFichierExcel = x.Fichier.chemin_fichier_excel,
+                        DateHeureIntegrationExcel = x.Fichier.date_heure_integration_excel,
+                        Integrateur = x.Utilisateur.nom_complet
                     })
                     .ToListAsync();
 
@@ -55,14 +60,18 @@ namespace DCCR_SERVER.Services.Archives
 
                 var fichiers = await requete
                     .OrderByDescending(f => f.date_heure_generation_xml)
-                    .Select(f => new ArchiveFichierXmlDto
+                    .Join(_contexte.utilisateurs,
+                        f => f.id_utilisateur_generateur_xml,
+                        u => u.matricule,
+                        (f, u) => new { Fichier = f, Utilisateur = u })
+                    .Select(x => new ArchiveFichierXmlDto
                     {
-                        IdFichierXml = f.id_fichier_xml,
-                        NomFichierCorrection = f.nom_fichier_correction,
-                        NomFichierSuppression = f.nom_fichier_suppression,
-                        DateHeureGenerationXml = f.date_heure_generation_xml,
-                        NomUtilisateurGenerateur = f.id_utilisateur_generateur_xml,
-                        NomFichierExcelSource = f.fichier_excel.nom_fichier_excel
+                        IdFichierXml = x.Fichier.id_fichier_xml,
+                        NomFichierCorrection = x.Fichier.nom_fichier_correction,
+                        NomFichierSuppression = x.Fichier.nom_fichier_suppression,
+                        DateHeureGenerationXml = x.Fichier.date_heure_generation_xml,
+                        NomUtilisateurGenerateur = x.Utilisateur.nom_complet,
+                        NomFichierExcelSource = x.Fichier.fichier_excel.nom_fichier_excel
                     })
                     .ToListAsync();
 
@@ -84,14 +93,26 @@ namespace DCCR_SERVER.Services.Archives
                 var credits = await requete
                     .OrderByDescending(c => c.date_declaration)
                     .ThenBy(c => c.numero_contrat_credit)
-                    .Select(c => new ArchiveCreditDto.ArchiveCreditListeDto
+                    .Join(_contexte.Set<TypeCrédit>(),
+                        c => c.type_credit,
+                        t => t.code,
+                        (c, t) => new { Credit = c, TypeLibelle = t.domaine })
+                    .Join(_contexte.Set<ActivitéCrédit>(),
+                        ct => ct.Credit.activite_credit,
+                        a => a.code,
+                        (ct, a) => new { ct.Credit, ct.TypeLibelle, ActiviteLibelle = a.domaine })
+                    .Join(_contexte.Set<SituationCrédit>(),
+                        cta => cta.Credit.situation_credit,
+                        s => s.code,
+                        (cta, s) => new { cta.Credit, cta.TypeLibelle, cta.ActiviteLibelle, SituationLibelle = s.domaine })
+                    .Select(x => new ArchiveCreditDto.ArchiveCreditListeDto
                     {
-                        num_contrat_credit = c.numero_contrat_credit,
-                        date_declaration = c.date_declaration,
-                        libelle_type_credit = c.type_credit,
-                        libelle_activite = c.activite_credit,
-                        libelle_situation = c.situation_credit,
-                        id_excel = c.id_excel,
+                        num_contrat_credit = x.Credit.numero_contrat_credit,
+                        date_declaration = x.Credit.date_declaration,
+                        libelle_type_credit = x.TypeLibelle,
+                        libelle_activite = x.ActiviteLibelle,
+                        libelle_situation = x.SituationLibelle,
+                        id_excel = x.Credit.id_excel,
                     })
                     .ToListAsync();
 
